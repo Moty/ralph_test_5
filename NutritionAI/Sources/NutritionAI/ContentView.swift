add24caa52004328252925ada55b23b68d0d43b1
Mordechai (Moty) Moshin <moty.moshin@gmail.com>
2026-01-12 19:24:58 -0800

    feat: US-014 - Add HomeView to TabView and update app flow

--- NutritionAI/Sources/NutritionAI/ContentView.swift
+++ NutritionAI/Sources/NutritionAI/ContentView.swift
@@ -1,10 +1,19 @@
 import SwiftUI
 
 public struct ContentView: View {
-    public init() {}
+    let apiService: APIService
+    
+    public init(apiService: APIService) {
+        self.apiService = apiService
+    }
     
     public var body: some View {
         TabView {
+            HomeView(apiService: apiService)
+                .tabItem {
+                    Label("Home", systemImage: "house.fill")
+                }
+            
             CameraView()
                 .tabItem {
                     Label("Camera", systemImage: "camera.fill")
--- NutritionAI/Sources/NutritionAI/NutritionAIApp.swift
+++ NutritionAI/Sources/NutritionAI/NutritionAIApp.swift
@@ -3,9 +3,22 @@ import SwiftUI
 #if !TESTING
 @main
 struct NutritionAIApp: App {
+    @StateObject private var authService = AuthService()
+    private let apiService = APIService()
+    
+    init() {
+        apiService.authService = authService
+    }
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            if authService.isAuthenticated {
+                ContentView(apiService: apiService)
+                    .environmentObject(authService)
+            } else {
+                LoginView(apiService: apiService)
+                    .environmentObject(authService)
+            }
         }
     }
 }
