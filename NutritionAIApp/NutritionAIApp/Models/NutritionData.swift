08ed68265dcd3cbfafe4b3a3e808672347e8dfc7
Mordechai (Moty) Moshin <moty.moshin@gmail.com>
2026-01-12 16:16:34 -0800

    feat: US-015 - Implement local storage service with Core Data

--- NutritionAIApp/NutritionAIApp/Models/NutritionData.swift
+++ NutritionAIApp/NutritionAIApp/Models/NutritionData.swift
@@ -35,5 +35,31 @@ struct FoodItem: Codable {
 struct MealAnalysis: Codable {
     let foods: [FoodItem]
     let totals: NutritionData
-    let timestamp: String
+    let timestamp: Date
+    
+    enum CodingKeys: String, CodingKey {
+        case foods, totals, timestamp
+    }
+    
+    init(foods: [FoodItem], totals: NutritionData, timestamp: Date) {
+        self.foods = foods
+        self.totals = totals
+        self.timestamp = timestamp
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        foods = try container.decode([FoodItem].self, forKey: .foods)
+        totals = try container.decode(NutritionData.self, forKey: .totals)
+        
+        // Try to decode as Date first, then fall back to String
+        if let date = try? container.decode(Date.self, forKey: .timestamp) {
+            timestamp = date
+        } else if let dateString = try? container.decode(String.self, forKey: .timestamp) {
+            let formatter = ISO8601DateFormatter()
+            timestamp = formatter.date(from: dateString) ?? Date()
+        } else {
+            timestamp = Date()
+        }
+    }
 }
